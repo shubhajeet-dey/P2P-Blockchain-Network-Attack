48d68c6069069729ca5fa994041cfbe0f591d360
onkarkadam158 <onkarkadam158@gmail.com>
2024-02-06 23:14:41 +0530

    Pulled from Main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from initialize import init_nodes
 
 if __name__ == "__main__":
 	
-	# Parse here
+	# Parse heregit 
 
 	# Initializing nodes and creating a P2P network
 	nodeArray = init_nodes(N, z0, z1, PoWI)
